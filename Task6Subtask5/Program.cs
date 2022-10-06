b3f279b6345a81ade2cb9dd74bdcb7de7849ddff
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-06 21:54:05 +0300

    Добавлено задание 3.6.5

---
+++ Task6Subtask5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int b = 5;
+        int k = -17;
+        b -= k;
+        Console.WriteLine("b - k = {0}", b);
+    }
+}
