dd34fcf293310dfb6010ed4476237f96fe21ce67
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-06 21:29:03 +0300

    Добавлено задание 3.5.3

---
+++ Task5Subtask3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string MyName = "Владимир";
+        byte age = 27;
+        Console.WriteLine("Привет, Мир");
+        Console.WriteLine();
+        Console.WriteLine($"Меня зовут {MyName}");
+        Console.WriteLine($"Мой возраст {age}");
+
+        Console.ReadKey();
+    }
+}
