8dabe59fc67fb48821ef1e01a9bc7f450ef9a23b
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-06 21:18:22 +0300

    Добавлено задание 3.4.2

---
+++ Task4Subtask2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        DaysOfWeek MyFavoriteDay;
+
+        MyFavoriteDay = DaysOfWeek.Friday;
+
+        Console.WriteLine(MyFavoriteDay);
+    }
+}
+
+enum DaysOfWeek : byte
+{
+    Monday = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
