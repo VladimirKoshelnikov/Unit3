bdd8c2ecefed8902fe3e45b5971bbd47d7753c37
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-06 22:16:19 +0300

    Добавлено последнее задание

---
+++ FinalTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FinalTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age;
+            bool iscorrect = int.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+
+            Console.Write("Enter your birthdate:");
+            string birthdate = Console.ReadLine();
+            
+            Console.WriteLine("Your birthdate is {0}", birthdate);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
