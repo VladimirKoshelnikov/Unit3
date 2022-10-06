fc1156df2862ab8efdb1b9abe797445552e41704
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-06 21:37:20 +0300

    Добавлено задание 3.5.5

---
+++ Task5Subtask5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string MyName = "Computer";
+        Console.WriteLine("Привет, человек");
+        Console.WriteLine("Меня зовут {0}", MyName);
+        Console.Write("А как тебя зовут? ");
+        string YourName = Console.ReadLine();
+        Console.Write("Рад встрече, {0} ", YourName);
+
+
+        //код, который позволит прочитать строку: "Цыган на цыпочках цыпленку цыкнул цыц" в переменную str
+
+        string str = Console.ReadLine();
+
+        Console.ReadKey();
+    }
+}
