59531937f581a8ce8eb5f37e82bd3c75578a946c
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-06 21:02:55 +0300

    Добавьте файлы проекта.

---
+++ Task3Subtask2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        string MyName = "Vladimir";
+        byte MyAge = 27;
+        bool HaveIApet = false;
+        double MyShoeSize = 43;
+
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("MyAge " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIApet);
+        Console.WriteLine("My shoe size is " + MyShoeSize);
+
+    }
+}
