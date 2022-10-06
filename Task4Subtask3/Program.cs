ec7fb06849ec5ddb592a5093b83d3243c0e5c534
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-06 21:22:06 +0300

    Добавлена задача 3.4.3

---
+++ Task4Subtask3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Semaphore MyFavoriteSColor;
+
+        MyFavoriteSColor = Semaphore.Red;
+
+        Console.WriteLine(MyFavoriteSColor);
+    }
+}
+
+enum Semaphore : int
+{
+    Red = 200,
+    Yellow  = 300,
+    Green = 400
+}
