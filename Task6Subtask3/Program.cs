48dcdf534b0f2ea2388b8a5d41b0dac43167222c
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-06 21:51:56 +0300

    Добавлено задание 3.6.3

---
+++ Task6Subtask3/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        double result = 5.0 / 2.0 * 3.0;
+        Console.WriteLine("5 / 2 * 3 = {0}", result);
+    }
+}
