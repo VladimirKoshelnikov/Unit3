4161364b2d7d84090e46bbabc600a7c03201049f
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-06 21:07:52 +0300

    Добавлено задание 3.2.3

---
+++ Task2Subtask3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string myName;
+        myName = "Vladimir";
+
+        Console.WriteLine(myName);
+
+        string myNameChanged;
+        myNameChanged = "Vladimir";
+
+        Console.WriteLine(myNameChanged);
+
+
+        Console.ReadKey();
+    }
+}
