9f13442d85e6b5be40dd1eef2b4af12931c84955
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-06 22:02:14 +0300

    Добавлено задание 3.6.8

---
+++ Task6Subtask8/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        double result = 10 % 3;
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
