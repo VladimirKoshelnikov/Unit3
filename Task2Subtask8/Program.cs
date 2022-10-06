a7253d502e2440f22b1d87b8a7d4ab7d9f8a84a5
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-06 21:14:25 +0300

    Добавлено задание 3.2.8

---
+++ Task2Subtask8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        const string MyName = "Владимир";
+
+        Console.WriteLine(MyName);
+
+        Console.WriteLine("\t Привет Мир");
+        Console.WriteLine("\t Мне 27 лет");
+        Console.WriteLine("\t My name is \n {0}", MyName);
+        Console.WriteLine("\u0040");
+        Console.WriteLine("\x23");
+        Console.ReadKey();
+
+    }
+}
